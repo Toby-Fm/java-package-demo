f1fa76fb1a1f3a327a2e28937c3908a98cca5fae
Toby-Fm <mail@toby-wichmann.de>
2025-08-04 20:05:46 +0200

    working?

--- src/main/java/de/io/github/tobyfm/App.java
+++ src/main/java/de/io/github/tobyfm/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from fremden Packet" );
+        System.out.println( "Hello World from fremden Packet | Das Paket wurde in github geuploaded" );
     }
 }
